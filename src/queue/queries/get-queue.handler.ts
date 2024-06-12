f93ec4bb498d64f0a14235310bc6559bb7222b42
Chris Gurung <chris.gurung@waterflow.technology>
2024-06-12 15:40:33 +0545

    chore: removed unused import

--- src/queue/queries/get-queue.handler.ts
+++ src/queue/queries/get-queue.handler.ts
@@ -1,15 +1,13 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { AmiService } from "src/ami/ami.service";
-import { QueueService } from "../queue.service";
 import { GetQueueQuery } from "./get-queue.query";
 
 @QueryHandler(GetQueueQuery)
 export class GetQueueQueryHandler implements IQueryHandler<GetQueueQuery> {
     constructor(
-        private queueService: QueueService,
         private amiService: AmiService
     ) { }
-    async execute(query: GetQueueQuery): Promise<any> {
+    async execute(query: GetQueueQuery) {
         return this.amiService.getAll()
     }
 }
