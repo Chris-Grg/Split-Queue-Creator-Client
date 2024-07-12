e7937cd409745e20ba13b5b30ed07c7b2a5311d2
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-12 10:53:16 +0545

    chore: added queue crud handlers to module providers

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AmiService } from 'src/ami/ami.service';
 import { AddToQueueCommandCommandHandler } from './commands/add-to-queue/add-to-queue.handler';
+import { CreateQueueCommandHandler } from './commands/create-queue/create-queue.handler';
+import { DeleteQueueCommandHandler } from './commands/delete-queue/delete-queue.handler';
 import { PauseQueueCommandHandler } from './commands/pause-or-unpause-queue/pause-or-unpause-queue.handler';
 import { RemoveQueueMemberHandler } from './commands/remove-queue-member/remove-queue.handler';
+import { UpdateQueueCommandHandler } from './commands/update-queue/update-queue.handler';
 import { GetQueueQueryHandler } from './queries/get-queue.handler';
 import { QueueController } from './queue.controller';
 import { QueueResolver } from './queue.resolver';
@@ -12,7 +15,11 @@ import { QueueService } from './queue.service';
 export const CommandHandlers = [
   AddToQueueCommandCommandHandler,
   PauseQueueCommandHandler,
-  RemoveQueueMemberHandler
+  RemoveQueueMemberHandler,
+  CreateQueueCommandHandler,
+  DeleteQueueCommandHandler,
+  UpdateQueueCommandHandler,
+
 ]
 export const QueryHandlers = [
   GetQueueQueryHandler
