f53448dd4c9fce5380dac103410518c1dc58a776
Chris Gurung <chris.gurung@waterflow.technology>
2024-07-12 10:54:02 +0545

    chore: added queue crud resolvers

--- src/queue/queue.resolver.ts
+++ src/queue/queue.resolver.ts
@@ -6,13 +6,18 @@ import {
     Resolver
 } from '@nestjs/graphql';
 import { AddToQueueCommand } from './commands/add-to-queue/add-to-queue.command';
+import { CreateQueueCommand } from './commands/create-queue/create-queue.command';
+import { DeleteQueueCommand } from './commands/delete-queue/delete-queue.command';
 import { PauseQueueCommand } from './commands/pause-or-unpause-queue/pause-or-unpause-queue.command';
 import { RemoveQueueMember } from './commands/remove-queue-member/remove-queue.command';
+import { UpdateQueueCommand } from './commands/update-queue/update-queue.command';
 import { GetQueueQuery } from './queries/get-queue.query';
 import { AddOrRemoveQueueMemberInput } from './types/add-or-remove-queue-input.schema';
+import { CreateOrRemoveQueueInput, DeleteQueueInput } from './types/create-or-remove-queue-input.schema';
 import { GetAllQueue } from './types/get-all-queue.schema';
 import { PauseQueueInput } from './types/pause-queue-input.schema';
 import { QueueResponse } from './types/queue-response.schema';
+import { UpdateQueueInput } from './types/update-queue-input.schema';
 
 @Resolver((of) => QueueResponse)
 export class QueueResolver {
@@ -29,6 +34,29 @@ export class QueueResolver {
     async getAllQueues() {
         const query = new GetQueueQuery();
         return await this.queryBus.execute(query);
+    }
+
+    @Mutation(() => String)
+    async createQueue(@Args('input') input: CreateOrRemoveQueueInput) {
+        const { queue_name, strategy, ringinuse, timeout, wrapuptime } = input
+        return await this.commandBus.execute(
+            new CreateQueueCommand(queue_name, strategy, ringinuse, timeout, wrapuptime),
+        );
+    }
+
+    @Mutation(() => String)
+    async deleteQueue(@Args('input') input: DeleteQueueInput) {
+        return await this.commandBus.execute(
+            new DeleteQueueCommand(input.queue_name),
+        );
+    }
+
+    @Mutation(() => String)
+    async updateQueue(@Args('input') input: UpdateQueueInput) {
+        const { name, queue_name, strategy, ringinuse, timeout, wrapuptime } = input
+        return await this.commandBus.execute(
+            new UpdateQueueCommand(name, queue_name, strategy, ringinuse, timeout, wrapuptime),
+        );
     }
 
     @Mutation(() => QueueResponse)
